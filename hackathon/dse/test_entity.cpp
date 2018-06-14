20214b6fe2fbcc9d631f6fc140815e8be58195b4
Simon Wright <wrightsgdev@gmail.com>
2018-06-14 17:08:35 +0200

    dse/test_entity: make test "get_resource_from_default_constructed" value parametrized

--- hackathon/dse/test_entity.cpp
+++ hackathon/dse/test_entity.cpp
@@ -7,10 +7,12 @@ TEST(entity, default_construct)
     entity e;
 }
 
-TEST(entity, get_resource_from_default_constructed)
+class test_param_resource : public ::testing::TestWithParam<resource> {};
+
+TEST_P(test_param_resource, get_resource_from_default_constructed)
 {
     entity e;
-    EXPECT_THROW(e.get_resource(resource::LUT), std::exception);
-    EXPECT_THROW(e.get_resource(resource::BRAM), std::exception);
-    EXPECT_THROW(e.get_resource(resource::DSP), std::exception);
+    EXPECT_THROW(e.get_resource(GetParam()), std::exception);
 }
+
+INSTANTIATE_TEST_CASE_P(all_resources, test_param_resource, ::testing::Values(resource::LUT, resource::BRAM, resource::DSP));
