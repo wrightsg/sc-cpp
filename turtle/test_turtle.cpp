9815b23ecbf4fa28036c0097dc3fe2f4d1ef9d69
Simon Wright <wrightsgdev@gmail.com>
2018-06-14 13:43:42 +0200

    test_turtle: add turtle_user, add failing test

--- turtle/test_turtle.cpp
+++ turtle/test_turtle.cpp
@@ -2,10 +2,41 @@
 
 #include "turtle_mock.h"
 
+class turtle_user
+{
+public:
+    explicit turtle_user (turtle_interface& turtle)
+        : _turtle(turtle)
+    {
+    }
+
+    void up (int times)
+    {
+        for (int n = 0; n < times; n++) {
+            _turtle.pen_up();
+        }
+    }
+
+private:
+    turtle_interface& _turtle;
+};
+
 TEST(turtle, pen_up)
 {
-    turtle_mock mock;
-    EXPECT_CALL(mock, pen_up()).Times(::testing::AtLeast(1));
+    turtle_mock turtle;
+    EXPECT_CALL(turtle, pen_up()).Times(::testing::AtLeast(1));
 
-    mock.pen_up();
+    turtle_user user(turtle);
+
+    user.up(2);
+}
+
+TEST(turtle, pen_down)
+{
+    turtle_mock turtle;
+    EXPECT_CALL(turtle, pen_down()).Times(::testing::AtLeast(1));
+
+    turtle_user user(turtle);
+
+    user.up(1);
 }
